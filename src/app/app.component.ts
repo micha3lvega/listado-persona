2665217f3cd427b15dbbf9a0cc5dc754fe66cfc2
Michael Vega <micha3lvega@gmail.com>
2020-06-20 21:15:25 -0500

    add primefaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Persona } from './persona.model';
+import { MessageService } from 'primeng/api';
+
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  providers: [MessageService],
 })
 export class AppComponent {
-  title = 'Listado de personas';
+  title: string = 'Listado de personas';
+
+  name: string;
+  invalidaName: boolean = false;
+
+  lastName: string;
+  invalidaLastName: boolean = false;
+
+  display = false;
+
+  personas: Persona[];
+
+  constructor(private messageService: MessageService) {
+    this.personas = [new Persona('Ana', 'Melo'), new Persona('Juan', 'Perez')];
+  }
+
+  showDialog() {
+    this.display = true;
+  }
+
+  onSubmit() {
+    var regex = /^[A-Za-z]+$/;
+
+    let valid = true;
+
+    if (!this.name || this.name.length < 3 || !this.name.match(regex)) {
+      this.invalidaName = true;
+      valid = false;
+    } else {
+      this.invalidaName = false;
+    }
+
+    if (
+      !this.lastName ||
+      this.lastName.length < 3 ||
+      !this.lastName.match(regex)
+    ) {
+      this.invalidaLastName = true;
+      valid = false;
+    } else {
+      this.invalidaLastName = false;
+    }
+
+    if (valid) {
+      this.personas.push(new Persona(this.name, this.lastName));
+      this.showCustom();
+      this.clearForm();
+    }
+  }
+
+  clearForm() {
+    this.display = false;
+    this.name = null;
+    this.lastName = null;
+  }
+
+  showCustom() {
+    this.messageService.add({
+      key: 'custom',
+      severity: 'info',
+      summary: 'Genial',
+      detail: 'Persona creada con exito',
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
+import { ToastModule } from 'primeng/toast';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { TabViewModule } from 'primeng/tabview';
+import { InputTextModule } from 'primeng/inputtext';
+import { CodeHighlighterModule } from 'primeng/codehighlighter';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    ToastModule,
+    ButtonModule,
+    FormsModule,
+    CommonModule,
+    DialogModule,
+    BrowserModule,
+    TabViewModule,
+    InputTextModule,
+    CodeHighlighterModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
